78a3b82ef52dffa399f6e895cf19628ab9b1401b
Ujjwal <ujjwalkr.maharana@gmail.com>
2022-08-10 10:14:11 +0900

    refactored code,added DI,enabled view binding

---
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.example.currencyconverter.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://openexchangerates.org/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}

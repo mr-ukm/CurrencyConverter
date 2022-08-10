08b9719079b2023b7629466192ae31676c27cdca
Ujjwal <ujjwalkr.maharana@gmail.com>
2022-08-10 11:29:50 +0900

    added toolbar with refresh icon

--- app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.currencyconverter.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.currencyconverter.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
 }
